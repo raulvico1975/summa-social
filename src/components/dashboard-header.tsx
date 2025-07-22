4764edd72d1fb4e29dc4500f8f3125e42102bacb
Raul Vico <raul@espanholraiz.com>
2025-07-22 16:48:43 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/dashboard-header.tsx
+++ src/components/dashboard-header.tsx
@@ -1,5 +1,7 @@
+
 'use client';
 
+import * as React from 'react';
 import { usePathname } from 'next/navigation';
 import { SidebarTrigger } from '@/components/ui/sidebar';
 import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from "@/components/ui/breadcrumb";
