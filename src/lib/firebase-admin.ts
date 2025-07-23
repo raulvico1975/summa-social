278d8e9962e4fc3ccbac6eb9a73e29f8a86da8dc
Raul Vico <raul@espanholraiz.com>
2025-07-23 10:51:41 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -5,17 +5,22 @@ import type { ServiceAccount } from 'firebase-admin';
 const serviceAccountString = process.env.FIREBASE_ADMIN_SDK_CONFIG;
 
 if (!serviceAccountString) {
-    throw new Error('The FIREBASE_ADMIN_SDK_CONFIG environment variable is not set. Please refer to the documentation to set it up.');
+    console.warn('The FIREBASE_ADMIN_SDK_CONFIG environment variable is not set. Authentication will not work on the server. Please refer to the documentation to set it up.');
 }
 
-let serviceAccount: ServiceAccount;
-try {
-    serviceAccount = JSON.parse(serviceAccountString);
-} catch (error) {
-    throw new Error('Failed to parse FIREBASE_ADMIN_SDK_CONFIG. Make sure it is a valid JSON string.');
+let serviceAccount: ServiceAccount | undefined;
+if (serviceAccountString) {
+    try {
+        serviceAccount = JSON.parse(serviceAccountString);
+    } catch (error) {
+        console.error('Failed to parse FIREBASE_ADMIN_SDK_CONFIG. Make sure it is a valid JSON string.');
+    }
 }
-
 
 if (!admin.apps.length) {
     admin.initializeApp({
-        credential: admin.
+        credential: serviceAccount ? admin.credential.cert(serviceAccount) : undefined,
+    });
+}
+
+export const authAdmin = admin.auth();
