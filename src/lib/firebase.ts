8836d73b7bd93064e0b664601c69ad5489945877
Raul Vico <raul@espanholraiz.com>
2025-08-05 14:49:17 +0000

    fa 5 minuts que està aixi

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,13 +4,14 @@ import { getAuth } from 'firebase/auth';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  projectId: "summa-social",
-  appId: "1:469685881071:web:97483f2003df7c2407731a",
-  storageBucket: "summa-social.appspot.com",
   apiKey: "AIzaSyAi_dEPmqHpbEdZH04pCnRRS85AlJ9Pe5g",
   authDomain: "summa-social.firebaseapp.com",
+  projectId: "summa-social",
+  storageBucket: "summa-social.appspot.com",
   messagingSenderId: "469685881071",
+  appId: "1:469685881071:web:97483f2003df7c2407731a"
 };
+
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
