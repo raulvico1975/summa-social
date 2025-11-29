eb4390f9868acd3148cc75c60a6bbfbc8fbf4b4c
Raul Vico <raul@espanholraiz.com>
2025-11-29 21:12:47 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "summa-social",
+  "appId": "1:469685881071:web:97483f2003df7c2407731a",
+  "apiKey": "AIzaSyAi_dEPmqHpbEdZH04pCnRRS85AlJ9Pe5g",
+  "authDomain": "summa-social.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "469685881071"
+};
