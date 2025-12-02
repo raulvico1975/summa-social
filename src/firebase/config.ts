7ba7aa7d81948ac7d1e8943ca57c0d5ede55baf3
Raul Vico <raul@espanholraiz.com>
2025-12-02 12:26:11 +0000

    Canvia-ho a: typescriptexport const firebaseConfig = {   "projectId": "s

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,7 +3,7 @@ export const firebaseConfig = {
   "appId": "1:469685881071:web:97483f2003df7c2407731a",
   "apiKey": "AIzaSyAi_dEPmqHpbEdZH04pCnRRS85AlJ9Pe5g",
   "authDomain": "summa-social.firebaseapp.com",
-  "storageBucket": "summa-social.appspot.com",
+  "storageBucket": "summa-social.firebasestorage.app",
   "measurementId": "",
   "messagingSenderId": "469685881071"
 };
