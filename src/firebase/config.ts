f9b52b986439b23eea4e87b4aa8ef9d7f5c97637
Raul Vico <raul@espanholraiz.com>
2025-12-02 12:12:02 +0000

    Trobat! Falta la línia storageBucket. 🔍
    🔧 Solució
    Afegeix la línia st

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,7 @@ export const firebaseConfig = {
   "appId": "1:469685881071:web:97483f2003df7c2407731a",
   "apiKey": "AIzaSyAi_dEPmqHpbEdZH04pCnRRS85AlJ9Pe5g",
   "authDomain": "summa-social.firebaseapp.com",
+  "storageBucket": "summa-social.appspot.com",
   "measurementId": "",
   "messagingSenderId": "469685881071"
 };
