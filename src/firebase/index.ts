c79eaeacad8c3b7404e234175c89a6110cce3043
Raul Vico <raul@espanholraiz.com>
2025-12-05 10:09:19 +0000

    Fix: Hardcodejar Firebase config

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,6 +1,13 @@
 'use client';
 
-import { firebaseConfig } from '@/firebase/config';
+const firebaseConfig = {
+  projectId: "summa-social",
+  appId: "1:469685881071:web:97483f2003df7c2407731a",
+  apiKey: "AIzaSyAi_dEPmqHpbEdZH04pCnRRS85AlJ9Pe5g",
+  authDomain: "summa-social.firebaseapp.com",
+  storageBucket: "summa-social.firebasestorage.app",
+  messagingSenderId: "469685881071"
+};
 import { initializeApp, getApps, getApp, FirebaseApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
